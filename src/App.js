de995ff53b5c115f7f75fe8254381c038ba7ca11
Hlib Hraif <hlib.hraif@protonmail.com>
2021-06-11 17:46:46 +0200

    Refactor App.js, add router

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
-import React, { useState } from 'react';
-import LoginForm from './login/LoginForm';
-import {LockClosedIcon} from "@heroicons/react/solid";
-import {BrowserRouter} from "react-router-dom"
+import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import LoginScreen from "./login/LoginScreen";
 
 function App() {
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123"
-  }
-
-  const [user, setUser] = useState({ email: ""});
-  const [error, setError] = useState("");
-
-  const Login = details => {
-    console.log(details);
-
-    if(details.email === adminUser.email && details.password === adminUser.password){
-      console.log("Logged in");
-      setUser({
-        name: details.name,
-        email: details.email
-      })
-    } else {
-      console.log("Details do not match!");
-      setError("Details do not match!");
-    }
-  }
-
-  const Logout = () => {
-    setUser({ name: "", email: ""});
-  }
-
   return (
-    <div className="endless-clouds fill-current min-h-screen min-w-screen flex flex-col items-center justify-evenly">
-      <h1 className="font-display text-6xl">Finance Manager</h1>
-      <LoginForm className="w-80" onSubmit={() => {}} />
-      <div className="h-12">
-
-      </div>
+    <div className="min-h-screen min-w-screen">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={LoginScreen}/>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/login/LoginScreen.js
@@ -0,0 +1,12 @@
+import LoginForm from "./LoginForm";
+import React from "react";
+
+const LoginScreen = (params) => <div className="endless-clouds fill-current flex flex-col items-center justify-evenly">
+  <h1 className="font-display text-6xl">Finance Manager</h1>
+  <LoginForm className="w-80" onSubmit={() => {}} />
+  <div className="h-12">
+
+  </div>
+</div>
+
+export default LoginScreen
